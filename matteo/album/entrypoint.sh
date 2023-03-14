2931de6cbae1e12b6eb2a8224d4b03d39c0933b0
Alex Carrega <contact@alexcarrega.com>
2023-03-14 17:29:24 +0100

    Minor changes

--- matteo/album/entrypoint.sh
+++ matteo/album/entrypoint.sh
@@ -6,10 +6,6 @@ python3 media_downloader.py
 
 fgallery -f photo gallery
 
-echo $FTP_HOST
-echo $FTP_USERNAME
-echo $FTP_PASSWORD
-
 ncftp -v -u $FTP_USERNAME -p $FTP_PASSWORD $FTP_HOST <<EOF
 rm -rf matteo
 mkdir matteo
