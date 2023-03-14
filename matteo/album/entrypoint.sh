59ee642b5aafd0fea36735677c0b32873dacdccd
Alex Carrega <contact@alexcarrega.com>
2023-03-14 14:44:40 +0100

    Major changes

--- matteo/album/entrypoint.sh
+++ matteo/album/entrypoint.sh
@@ -4,4 +4,4 @@ python3 media_downloader.py
 
 fgallery photo gallery
 
-ncftpput -R -v -u "www@alexcarrega.netsons.org" ftp.alexcarrega.netsons.org /matteo gallery/*
+ncftpput -R -v -u $FTP_USERNAME -p $FTP_PASSWORD $FTP_HOST /matteo gallery/*
