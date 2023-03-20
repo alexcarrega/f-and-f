c15b589578e6c8796e465e86990eafc6bd74b686
Alex Carrega <contact@alexcarrega.com>
2023-03-20 14:32:22 +0100

    Minor changes

--- matteo/album/entrypoint.sh
+++ matteo/album/entrypoint.sh
@@ -7,6 +7,7 @@ python3 media_downloader.py
 fgallery -f photo gallery
 
 curl --user "$WEBDISK_USERNAME:$WEBDISK_PASSWORD" -X DELETE "$WEBDISK_URL/matteo"
+curl --user "$WEBDISK_USERNAME:$WEBDISK_PASSWORD" -X MKCOL "$WEBDISK_URL/matteo"
 
 ncftpput -R -v -u $FTP_USERNAME -p $FTP_PASSWORD $FTP_HOST /matteo gallery/*
 ncftpput -R -v -u $FTP_USERNAME -p $FTP_PASSWORD $FTP_HOST /matteo .ht*
