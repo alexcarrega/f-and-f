0b439a6b2b8ce7b9bccbf84fa70c1157636291df
Alex Carrega <contact@alexcarrega.com>
2025-01-15 16:22:26 +0100

    Automatic commit with GitManager

--- matteo/album/entrypoint.sh
+++ matteo/album/entrypoint.sh
@@ -4,10 +4,13 @@ cd /telegram_media_downloader
 
 python3 media_downloader.py
 
-fgallery -f photo gallery
+cd photo
+llgal --title "Matteo's Album"
 
 curl --user "$WEBDISK_USERNAME:$WEBDISK_PASSWORD" -X DELETE "$WEBDISK_URL/matteo"
 curl --user "$WEBDISK_USERNAME:$WEBDISK_PASSWORD" -X MKCOL "$WEBDISK_URL/matteo"
 
-ncftpput -R -v -u $FTP_USERNAME -p $FTP_PASSWORD $FTP_HOST /matteo gallery/*
+cd ..
+
+ncftpput -R -v -u $FTP_USERNAME -p $FTP_PASSWORD $FTP_HOST /matteo photo/*
 ncftpput -R -v -u $FTP_USERNAME -p $FTP_PASSWORD $FTP_HOST /matteo .ht*
